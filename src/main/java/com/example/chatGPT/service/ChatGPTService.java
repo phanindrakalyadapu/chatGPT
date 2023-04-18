40b93d49f452cb01c92db4ca3d0a2f819bbdff85
phanindrakalyadapu <113396039+phanindrakalyadapu@users.noreply.github.com>
2023-04-18 11:30:54 -0600

    Please mention your API_KEY

--- src/main/java/com/example/chatGPT/service/ChatGPTService.java
+++ src/main/java/com/example/chatGPT/service/ChatGPTService.java
@@ -7,7 +7,7 @@ import kong.unirest.json.JSONObject;
 import java.util.Collections;
 
 public class ChatGPTService {
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "xxxxxxx";
     private static final String API_ENDPOINT = "https://api.openai.com/v1/completions";
     public String getAnswer(String question) {
         String prompt = "Answer the following question: " + question;
